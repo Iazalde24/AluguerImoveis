97e26e4b436b3060ffeba2f7b131412eaaf02c65
unknown <ijunior@unilurio.ac.mz>
2024-10-11 17:03:29 +0200

    Reiniciando o projecto

---
+++ src/main/java/com/pw/demo/AluguerDeImoveisApplication.java
@@ -0,0 +1,13 @@
+package com.pw.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluguerDeImoveisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluguerDeImoveisApplication.class, args);
+	}
+
+}
